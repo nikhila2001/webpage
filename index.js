a1dd35a76b97ace9baac4053370ac5289f4e48c4
sargam nikhila <nsargam58@gmail.com>
2024-01-07 01:22:52 +0530

    carousel

--- index.js
+++ index.js
@@ -19,3 +19,24 @@ function myFunction() {
     }
 }
 
+$('.owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: [
+    "<i class='fa fa-caret-left'></i>",
+    "<i class='fa fa-caret-right'></i>"
+  ],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
